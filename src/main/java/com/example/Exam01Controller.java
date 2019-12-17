6b2f6a1883683bcb8b92859678ea35c0deaf847b
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-12-17 10:08:11 +0900

    入力したものを表示させる機能が完成しました。
    入力画面の作成
    
    結果画面作成
    
    完成

---
+++ src/main/java/com/example/Exam01Controller.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam01")
+public class Exam01Controller {
+
+	@RequestMapping("")
+	public String init() {
+		System.out.println("a");
+		return "exam01";
+	}
+	
+
+	@RequestMapping("/input")
+	public String input(String name, Model model) {
+		System.out.println("b");
+		model.addAttribute("name", name);
+		return "exam01-result";
+	}
+	
+}
